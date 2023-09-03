1e3dfac29bdfdbcd77a09e2cc6db3f6ad0a040ca
Marta Nowak <138518482+martapuszkonowak@users.noreply.github.com>
2023-09-03 20:14:00 +0100

    Migrated Vanilla App to React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+import Weather from "./Weather";
+import Footer from "./Footer";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Weather />
+      <Footer />
+    </div>
+  );
+}
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <small>
+        <a href="#123">Open-source code</a>, by Marta Nowak
+      </small>
+    </div>
+  );
+}
